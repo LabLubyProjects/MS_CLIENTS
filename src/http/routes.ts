2f4da00e39f00ed5cd2333815f451b54452e1716
Breno Vergilio <bjvergilio47@gmail.com>
2022-08-02 10:58:10 -0400

    ✨ performing transactions between users

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,6 +2,7 @@ import FindAllClients from "@src/usecases/findAllClients";
 import FindClientByID from "@src/usecases/findClientByID";
 import DeleteClient from "@src/usecases/deleteClient";
 import { Router, Request, Response } from "express";
+import PerformTransaction from "@src/usecases/performTransaction";
 
 const router: Router = Router();
 
@@ -15,6 +16,18 @@ router.get("/clients/:id", async (req: Request, res: Response) => {
   const client = await new FindClientByID().execute(clientId);
   if (!client) return res.status(404).json({ message: "Client not found" });
   return res.json(client);
+});
+
+router.post("/transaction", async (req: Request, res: Response) => {
+  const transactionBody = req.body();
+  const resultOfTransaction = await new PerformTransaction().execute(
+    JSON.stringify(transactionBody)
+  );
+
+  if (!resultOfTransaction)
+    return res.status(400).json({ message: "Transaction failed" });
+
+  return res.json({ message: "Transaction completed successfully" });
 });
 
 router.delete("/clients/:id", async (req: Request, res: Response) => {
---
+++ src/usecases/performTransaction.ts
@@ -0,0 +1,51 @@
+import { PrismaClient } from "@prisma/client";
+import UseCase from "./UseCase";
+
+interface PerformTransactionInput {
+  sourceUserId: string;
+  targetUserId: string;
+  amount: number;
+}
+
+export default class PerformTransaction implements UseCase {
+  async execute(transactionData: string) {
+    const input: PerformTransactionInput = JSON.parse(transactionData);
+    const prisma = new PrismaClient();
+    const source = await prisma.client.findUnique({
+      where: { id: input.sourceUserId },
+    });
+
+    if (!source) return false;
+
+    const target = await prisma.client.findUnique({
+      where: { id: input.targetUserId },
+    });
+
+    if (!target) return false;
+
+    try {
+      const updateSourceBalance = prisma.client.update({
+        where: {
+          id: input.sourceUserId,
+        },
+        data: {
+          current_balance: Number(source.current_balance) - input.amount,
+        },
+      });
+
+      const updateTargetBalance = prisma.client.update({
+        where: {
+          id: input.targetUserId,
+        },
+        data: {
+          current_balance: Number(source.current_balance) + input.amount,
+        },
+      });
+
+      await prisma.$transaction([updateSourceBalance, updateTargetBalance]);
+    } catch (error) {
+      return false;
+    }
+    return true;
+  }
+}

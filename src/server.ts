874c1d43808862ca7beba2ce35c02cc4728e2f6c
Breno Vergilio <bjvergilio47@gmail.com>
2022-08-05 00:34:13 -0400

    🔥 removing graceful shutdown

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,8 @@
 import App from "./http/app";
 import dotenv from "dotenv";
-import { consume, KafkaSingleton } from "./messaging/kafka";
+import { consume } from "./messaging/kafka";
 
 dotenv.config();
 
 consume(["store-client", "delete-client", "update-client"]);
-const server = new App().run(Number(process.env.PORT) || 3000);
-
-process.on("SIGTERM", () => {});
-
-(async (): Promise<void> => {
-  const exitSignals: NodeJS.Signals[] = ["SIGINT", "SIGTERM", "SIGQUIT"];
-  exitSignals.map((sig) =>
-    process.on(sig, async () => {
-      server.close(() => {
-        console.log("Express Server Closed");
-      });
-      await KafkaSingleton.shutdown();
-    })
-  );
-})();
+new App().run(Number(process.env.PORT) || 3000);
